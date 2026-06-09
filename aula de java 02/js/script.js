aec5668332bf7ece60c69e9873b36bb9fa4e2e87
Enzobenjamin10 <enzobdop@gmail.com>
2026-06-09 17:23:25 -0300

    Add files via upload

---
+++ aula de java 02/js/script.js
@@ -0,0 +1,142 @@
+/**let nota = Number(prompt("informe uma nota"))
+
+if(nota >=90 ){
+    alert("o conceito é A")
+}else if(nota >=80){
+    alert("o conceito é B")
+}else{
+    alert("o conceito é C")
+}
+     
+
+let numero = Number(prompt("informe um numero de 1 a 7: "));
+
+switch (numero) {
+    case 1: alert("domingo")
+        break;
+    case 2: alert("segunda")
+        break;
+    case 3: alert("terça")
+        break;
+    case 4: alert("quarta")
+        break;
+    case 5: alert("quinta")
+        break;
+    case 6: alert("sexta")
+        break;
+    case 7: alert("sabado")
+        break;
+    default: alert("dia invalido")
+}
+    
+
+let temperatura = Number(prompt("informe a temperatura: "))
+
+let temp = temperatura>25?"quente":"frio";
+alert(temp)
+
+
+let maiorIdade = prompt("é maior de idade")==="true"
+
+let carteira = prompt("tem carteira de motorista")=== "true"
+
+if(maiorIdade >=18 && carteira){
+    alert("pode dirigir")
+}else{
+    alert("nao pode dirigir")
+}
+    
+
+let numero = Number(prompt("informe um numero"))
+
+if(numero >10 && numero <=20){
+    alert("dentro do intervalo!")
+}else{
+    alert("fora do intervalo!")
+}
+    
+let cor= prompt("informe uma cor").
+toLowerCase()
+switch(cor){
+    case "Vermelho": alert("Pare");
+    break
+    case"amarelo": alert("Atenção");
+    break;
+    case"verde": alert("Siga");
+    break;
+   
+*/
+/** 
+Decisão com if/else: Crie uma variável com idade
+ e use if/else para decidir se é maior de idade ou não
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ /**Atividade 
+
+1-
+
+ let idade = Number(prompt("idade"));
+ if(idade >=18){
+    alert("maior de idade");
+ } else {
+    alert("menor de idade");
+ }
+
+*/
+
+
+/**
+ 2-
+
+ let numero = Number(prompt("numero de 1 a 3:"));
+ switch (numero){
+    case 1: alert("Baixo");
+    break;
+    case 2: alert("Médio");
+    break;
+    case 3: alert("Alto");
+    break;
+    default("numero invalido");
+ }
+
+*/
+
+/**
+3-
+
+  let numero = Number(prompt("digite"));
+  let resultado = numero >= 8? "positivo" :"negativo";
+  alert(resultado);
+
+*/
+/**
+4-
+ 
+let idade = Number(prompt("idade"));
+let nacionalidade = prompt("nacionalidade");
+if (idade >=16 && nacionalidade === "brasileiro"){
+    alert("pode votar");
+}else {
+    alert("não pode votar");
+}
+*/
+
+  
